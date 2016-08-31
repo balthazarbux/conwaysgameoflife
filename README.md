6ac05ac98af0e6801e9697875bb0eb9605477336
balthazarbux <balthazarbux@fastmail.com>
2016-08-30 21:51:48 -0500

    Minor refactors and new files   - added README   - added requirements   - reduced code

---
+++ README.md
@@ -0,0 +1,20 @@
+# Conway's Game of Life
+
+This game literally has trillions of python implementations and they're probably
+better than mine, but fuck it, I'm bored.
+
+## Gameplay
+
+`./main.py`
+
+## Random seed
+
+manually set the random seed (self.seed on line 11 of game.py) with one of these
+for some semi-interesting results:
+
+    647, 238, 859, 554, 948, 917, 477, 342, 287, 305, 141, 56, 94, 125,
+    857, 837, 159, 649
+
+### Requirements
+
+  - numpy
--- game.py
+++ game.py
@@ -1,16 +1,19 @@
-import numpy as np
+import os
 import random
 import time
-import os
+import numpy as np
 
 
 class ConwaysGameOfLife(object):
     def __init__(self, *args, **kwargs):
         self.size = 20
+        self.tick = 0.5
+        self.seed = random.randrange(0, 1000)
         self.cells = np.zeros((self.size, self.size), dtype=np.int8)
-        self.seed_cells()
+        self.random_seed_cells()
 
-    def seed_cells(self):
+    def random_seed_cells(self):
+        random.seed(self.seed)
         s = self.size - 1
         for n in range(self.size * 2):
             row = random.randint(0, s)
@@ -19,47 +22,47 @@ class ConwaysGameOfLife(object):
 
     def run(self):
         running = True
-
         while running:
-            self.show_cells()
-            self.update_cells()
-            time.sleep(0.5)
-            os.system('clear')
+            self.update()
+            self.display()
+            time.sleep(self.tick)
+            running = np.count_nonzero(self.cells) > 0
 
-    def update_cells(self):
+    def update(self):
+        new_cells = np.copy(self.cells)
         for y, row in enumerate(self.cells):
             for x, col in enumerate(row):
-                # check neighbors
                 yh = y - 1
                 yi = y + 1
                 xh = x - 1
                 xi = x + 1
-
                 neighbors = 0
-                if yh >= 0:
-                    neighbors += self.cells[yh, x]
-                if yi < self.size:
-                    neighbors += self.cells[yi, x]
-                if xh >= 0:
-                    neighbors += self.cells[y, xh]
-                if xi < self.size:
-                    neighbors += self.cells[y, xi]
-                if yh >= 0 and xh >= 0:
-                    neighbors += self.cells[yh, xh]
-                if yi < self.size and xh >= 0:
-                    neighbors += self.cells[yi, xh]
-                if yh >= 0 and xi < self.size:
-                    neighbors += self.cells[yh, xi]
-                if yi < self.size and xi < self.size:
-                    neighbors += self.cells[yi, xi]
+                for c in (
+                    (yh, x), (yi, x),
+                    (y, xh), (y, xi),
+                    (yh, xh), (yi, xh),
+                    (yh, xi), (yi, xi)
+                ):
+                    neighbors += self.get_cell(c)
 
                 if neighbors < 2 or neighbors > 3:
-                    self.cells[y, x] = 0
+                    new_cells[y, x] = 0
                 elif neighbors == 3:
-                    self.cells[y, x] = 1
+                    new_cells[y, x] = 1
+        self.cells = new_cells
 
-    def show_cells(self):
+    def get_cell(self, c):
+        y, x = c
+        res = 0
+        if x >= 0 and x < self.size and y >= 0 and y < self.size:
+            res = self.cells[y, x]
+        return res
+
+    def display(self):
+        os.system('clear')
+        print "Seed: %s" % self.seed
         for row in self.cells:
             for col in row:
-                print col,
+                 output = '*' if col == 1 else ' '
+                 print output,
             print
